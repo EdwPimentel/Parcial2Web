46dc2e6737bbd5d3d20acb84b436e94b91234264
Rbll18 <regisbaez18@gmail.com>
2018-07-19 15:05:08 -0400

    check reacciones

--- src/main/java/modelo/Megusta.java
+++ src/main/java/modelo/Megusta.java
@@ -1,13 +1,21 @@
 package modelo;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Megusta {
+    @Id
     private long id;
+    @OneToOne
     private Image image;
+    @OneToOne
     private Comentario comentario;
+    @OneToOne
     private Album album;
+    @OneToOne
     private Post post;
+    //check
     private boolean megusta;
 }
