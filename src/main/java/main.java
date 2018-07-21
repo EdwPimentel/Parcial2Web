f27002fea4b2721f77c4a7701eac8bd375e5a3fe
EdwPimentel <edssowpg13@hotmail.com>
2018-07-20 22:40:40 -0400

    algunos cambios de template y setup para la base de datos en el main

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,7 +1,14 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.io.IOException;
 import java.util.List;
 
 public class main {
   //  ClientConfiguration streamConfig = new ClientConfiguration();
+  public static void main(String[] args){
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("perunit");
+    EntityManager em = emf.createEntityManager();
+  }
  //   StreamClient streamClient = new StreamClientImpl(streamConfig, "jmcze9f5wmsr", "dpujgvytx5jt2yzw6xc9zesnvdcg754j3tzym3qbm7ncgtpyy8yt4ztrqfkgghjg");
 }
